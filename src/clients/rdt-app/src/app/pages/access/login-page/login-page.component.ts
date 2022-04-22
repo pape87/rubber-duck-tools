c2af10a0eac0a409aa7cadec54db010735a2c71e
pape87 <perotti.luca.87@gmail.com>
2022-04-22 18:26:50 +0200

    chore(rdt-app): remove unused imports

--- src/clients/rdt-app/src/app/pages/access/login-page/login-page.component.ts
+++ src/clients/rdt-app/src/app/pages/access/login-page/login-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-// import { HttpClient } from "@angular/common/http";
 import { AuthService } from 'src/app/_domain/auth/auth.service';
 
 @Component({
