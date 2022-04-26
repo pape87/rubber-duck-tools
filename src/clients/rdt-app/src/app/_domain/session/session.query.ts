1281aaf80714c548dbdccd344172c7e4183008b7
pape87 <perotti.luca.87@gmail.com>
2022-04-26 12:17:23 +0200

    feat(rdt-app): add user component

--- src/clients/rdt-app/src/app/_domain/session/session.query.ts
+++ src/clients/rdt-app/src/app/_domain/session/session.query.ts
@@ -7,8 +7,7 @@ import { SessionStore } from "./session.store";
 @Injectable({ providedIn: "root" })
 export class SessionQuery extends Query<Session> {
 
-  accessToken$ = this.select(state => state.access_token || null);
-  idToken$ = this.select(state => state.id_token || null);
+  token$ = this.select(state => state || null);
 
   constructor(protected override store: SessionStore) {
     super(store);
---
+++ src/clients/rdt-app/src/app/components/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { Subscription, tap } from "rxjs";
+import { Session } from "src/app/_domain/session/session.model";
+import { SessionQuery } from "src/app/_domain/session/session.query";
+
+@Component({
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"]
+})
+export class UserComponent implements OnInit {
+
+  private subscription: Subscription | null = null;
+  private token$ = this.session.select();
+  
+  public token: Session | null = null;
+
+
+  constructor(private session: SessionQuery) { }
+
+  ngOnInit(): void {
+    this.subscription = this.session.token$.pipe(tap((token) => {
+      this.token = token;
+    })).subscribe();
+  }
+
+}
